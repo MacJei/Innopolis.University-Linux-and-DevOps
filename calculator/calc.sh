9fba3f45d94aae7bfbef54d672258900a42868e6
DevOps Engeneer <devops@localhost.localdomain>
2021-10-10 23:48:48 +0300

    First commit

---
+++ calculator/calc.sh
@@ -0,0 +1,20 @@
+FILE_1="$1"
+FILE_2="$2"
+
+if [[ -z $FILE_1 ]]; then
+	echo "Параметры не указаны"
+	echo "Пожалуйста, передайте один или два файла с арифметическим выражением, например:"
+	echo "./calc.sh file1.txt file2.txt"
+else
+	result_1=$(($(cat "$FILE_1")))
+	if [[ ! -z "$FILE_2" ]]; then
+		result_2=$(($(cat "$FILE_2")))
+		if [ $result_2 -eq $result_1 ] || [ $result_2 -gt $result_1 ]; then
+			echo $result_2
+		else
+			echo $result_1
+		fi
+	else
+		echo $result_1
+	fi
+fi
